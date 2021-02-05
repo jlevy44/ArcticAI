353623f8b76edd1fdd0dc9cb8f75a18381b0954f
jlevy44 <joshualevy44@berkeley.edu>
2021-02-05 16:49:04 -0500

    Update arctic_ai_dag.py

--- workflow/arctic_ai_dag.py
+++ workflow/arctic_ai_dag.py
@@ -45,6 +45,7 @@ def create_dag(patient):
         'start_date': dt.datetime(2017, 6, 1),
         'retries': 1,
         'retry_delay': dt.timedelta(minutes=15),
+        'run_as_user': 'tmp_user'
     }
     dag= DAG(f'airflow_arctic_ai_patient_{patient}',
              default_args=default_args,
@@ -114,3 +115,4 @@ def create_dag(patient):
 patients=np.unique(np.vectorize(lambda x: os.path.basename(x)[:6])(glob.glob("inputs/*.npy")))
 for patient in patients:
     globals()[patient] = create_dag(patient)
+    # instead read a database
